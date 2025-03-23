1b5534917bb397dc155dc9236dc86b84d3d3e0a3
Narayana Gupta <narayanagupta14@gmail.com>
2025-03-23 10:50:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_AUTH_DOMAIN",
-databaseURL: "YOUR_DATABASE_URL",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_STORAGE_BUCKET",
-messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-appId: "YOUR_APP_ID"
+apiKey: "AIzaSyCje7mNp9y6TO6qBGm7iEQx2XNd2ZOrz9o",
+authDomain: "feedback-system-7ec2f.firebaseapp.com",
+databaseURL: "https://feedback-system-7ec2f-default-rtdb.firebaseio.com",
+projectId: "feedback-system-7ec2f",
+storageBucket: "feedback-system-7ec2f.firebasestorage.app",
+messagingSenderId: "766852655039",
+appId: "1:766852655039:web:6a4c85015031303d6b630a",
+measurementId: "G-CTCFTB2V7D"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
